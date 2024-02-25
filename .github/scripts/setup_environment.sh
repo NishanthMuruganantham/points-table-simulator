1b061a5bf087b909a7457961577e2cfed28bbb53
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2024-02-25 23:26:51 +0530

    pytest has been installed

--- .github/scripts/setup_environment.sh
+++ .github/scripts/setup_environment.sh
@@ -6,6 +6,8 @@ if [ "$1" == 'pylint_check' ]; then
     pip install pylint
 elif [ "$1" == 'pyright_check' ]; then
     pip install pyright
+elif [ "$1" == 'unittest_check' ]; then
+    pip install pytest
 else
     pip install isort
 fi
