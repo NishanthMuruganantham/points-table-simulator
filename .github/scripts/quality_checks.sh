62998acbc8796cfd0113ab0a97a9fcc3e71bfa35
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2024-02-25 22:41:08 +0530

    pyright check has been added

--- .github/scripts/quality_checks.sh
+++ .github/scripts/quality_checks.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" == 'pylint_check' ]; then
     venv/bin/python -m pylint $(git ls-files 'src/*.py')
+elif [ "$1" == 'pyright_check' ]; then
+    venv/bin/python -m pyright $(git ls-files 'src/*.py')
 else
     venv/bin/python -m isort --check $(git ls-files 'src/*.py')
 fi
--- .github/scripts/setup_environment.sh
+++ .github/scripts/setup_environment.sh
@@ -7,6 +7,8 @@ bash create_venv.sh
 source venv/bin/activate
 if [ "$1" == 'pylint_check' ]; then
     pip install pylint
+elif [ "$1" == 'pyright_check' ]; then
+    pip install pyright
 else
     pip install isort
 fi
