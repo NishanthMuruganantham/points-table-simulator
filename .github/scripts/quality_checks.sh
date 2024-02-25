2263de014adaf4174f2eb226fe066d3836916db0
Nishanth Muruganantham <82966884+NishanthMuruganantham@users.noreply.github.com>
2024-02-25 23:06:45 +0530

    Pyright check has been added in the quality_check pipeline (#22)

--- .github/scripts/quality_checks.sh
+++ .github/scripts/quality_checks.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+source venv/bin/activate
 if [ "$1" == 'pylint_check' ]; then
     venv/bin/python -m pylint $(git ls-files 'src/*.py')
+elif [ "$1" == 'pyright_check' ]; then
+    venv/bin/python -m pyright $(git ls-files 'src/*.py')
 else
     venv/bin/python -m isort --check $(git ls-files 'src/*.py')
 fi
--- .github/scripts/setup_environment.sh
+++ .github/scripts/setup_environment.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Create virtual environment
 bash create_venv.sh
-
-# Install dependencies based on check type
 source venv/bin/activate
 if [ "$1" == 'pylint_check' ]; then
     pip install pylint
+elif [ "$1" == 'pyright_check' ]; then
+    pip install pyright
 else
     pip install isort
 fi
